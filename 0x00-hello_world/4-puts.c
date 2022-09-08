a2201bdd9874021c92952d894b11d5d7f7779322
Aminat27 <adebayoaminat27@gmail.com>
2022-09-08 13:56:15 +0000

    added 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	/*string intialization*/
+	char program[] = "Programming is like building a multilingual puzzle \n";
+	puts(program); /* writing string to stdout*/
+	return 0;
+}
