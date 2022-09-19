740471f5783921da085c6ab5f90de99850b0940c
Aminat27 <adebayoaminat27@gmail.com>
2022-09-19 13:55:19 +0000

    a function that prints a string, followed by a new line

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,8 +4,8 @@
  * _puts - a function that prints a string, followed by a new line, to stdout.
  * @str: the character to print
  *
-*/
+ */
 void _puts(char *str)
 {
-        puts(str);
+	puts(str);
 }
