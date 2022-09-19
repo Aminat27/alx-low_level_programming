39504caaa0cb330a0fc1768d7caa5e260120247c
Aminat27 <adebayoaminat27@gmail.com>
2022-09-19 11:13:26 +0000

    using pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * reset_to_98 - check the code 
- *
- * Return: Always 0.
+ * reset_to_98 - check the code
+ * @n: input integer value
+ * Return: nothing.
  */
 void reset_to_98(int *n)
 {
