7d2881521f755d73c3355de57eb5d8a506bc9185
Aminat27 <adebayoaminat27@gmail.com>
2022-10-01 10:07:26 +0000

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main- program that prints its name, followed by a new line.
+ * @argc: input argument
+ * @argv: input string of arg
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, add = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
