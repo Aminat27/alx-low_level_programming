d7320d37accd7a46869caf051a7ec91569a82865
Aminat27 <adebayoaminat27@gmail.com>
2022-10-04 08:16:59 +0000

    an array of char

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - a function returns a pointer to a new string which is
+ * a duplicate of the string str.
+ * @str: input string
+ * Return: pointer to string
+ */
+char *_strdup(char *str)
+{
+
+}
