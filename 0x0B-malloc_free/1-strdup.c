8fcd0df9f7852848b1fbfcb39a558177e785fb80
Aminat27 <adebayoaminat27@gmail.com>
2022-10-04 08:35:12 +0000

    duplicate string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,5 +10,25 @@
  */
 char *_strdup(char *str)
 {
+	char *pi;
+	unsigned int a, b;
 
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		;
+	}
+	pi = (char *)malloc(sizeof(char) * (a + 1));
+	if (pi == NULL)
+	{
+		return (NULL);
+	}
+	for (b = 0; b <= a; b++)
+	{
+		pi[b] = str[b];
+	}
+	return (pi);
 }
