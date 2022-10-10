e487948262066d81ed044346e7c98425330bf460
Aminat27 <adebayoaminat27@gmail.com>
2022-10-10 13:27:42 +0000

    print d

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		if (!(d->name))
-			printf("nil");
+			printf("Name: (nil)\n");
 
 		else
 			printf("Name: %s\n", d->name);
@@ -19,9 +19,10 @@ void print_dog(struct dog *d)
 		printf("Age: %f\n ", d->age);
 
 		if (!(d->owner))
-			printf("nil");
+			printf("Owner: (nil)\n");
 
 		else
 			printf("Owner: %s\n", d->owner);
+
 	}
 }
