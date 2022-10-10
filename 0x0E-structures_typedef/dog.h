f2434662cddd8222d1b778da2ce0708bb8f5b0cf
Aminat27 <adebayoaminat27@gmail.com>
2022-10-10 12:04:31 +0000

    added header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+/**
+ * struct dog- details about my dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+dog_t *new_dog(char *name, float age, char *owner);
+
+#endif
