7b97a5d0cd424ab9477acdf06a19f463c77fbf44
Aminat27 <adebayoaminat27@gmail.com>
2022-10-10 13:50:53 +0000

    free

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,7 +18,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (my_dog == NULL)
 		return (NULL);
 	my_dog->name = name;
-	my_dog->age = age;
 	my_dog->owner = owner;
-	return my_dog;
+	return (my_dog);
 }
