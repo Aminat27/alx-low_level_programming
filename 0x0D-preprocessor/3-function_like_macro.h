a423a8959e7ad83b09c5fa58cc0b9719656256fb
Aminat27 <adebayoaminat27@gmail.com>
2022-10-10 16:57:45 +0000

    task 3,4

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,5 @@
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
+
+#define ABS(s) ((s) < (0) ? (s) * (-1) : (s))
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,5 @@
+#ifndef _SUM_
+#define _SUM_
+
+#define SUM(a, b) ((a) + (b))
+#endif
