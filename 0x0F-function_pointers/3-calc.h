a4d4c4cdbfa90dbd562dc6e4ce89529059d9abb9
Aminat27 <adebayoaminat27@gmail.com>
2022-10-12 14:08:52 +0000

    done 3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,7 @@
 #ifndef _3_CALC_H
 #define _3_CALC_H
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * struct op - Struct op
  *
@@ -13,7 +14,7 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
-
+int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+/**
+ * op_add - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: sum of two numbers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtract two numbers
+ * @a: first number
+ * @b: second number
+ * Return: difference of two numbers
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - mutiply two numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: product of two numbers.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divdes two numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: division of two numbers.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modulus two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: remainder after division
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
