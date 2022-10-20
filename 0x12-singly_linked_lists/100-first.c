74681a3e894b6f8f9e2639268f86b2e3d3c276a0
Aminat27 <adebayoaminat27@gmail.com>
2022-10-20 16:26:44 +0000

    before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * pre_main - check the code
+ *
+ * Return: Always 0.
+ */
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
